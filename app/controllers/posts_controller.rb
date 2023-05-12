21a4b26fc4a266f653c1617d5a153440c89eddb2
John Adibe <johnadibe123@gmail.com>
2023-05-11 21:57:58 -0800

    Fix NoMethodError in post show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,6 @@ class PostsController < ApplicationController
 
   def show
     @user = User.find(params[:user_id])
-    @posts = @user.posts.find(params[:id])
+    @post = @user.posts.find(params[:id])
   end
 end
