8eebd63dc01df26468b8a99d2c1dba800e366bdc
John Adibe <johnadibe123@gmail.com>
2023-05-11 18:01:53 -0800

    Fix rails db:seed error as requested by the code reviewer

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,32 +7,28 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # Create a user
-first_user = User.create(name: 'Lillian', photo: 'https://media.gettyimages.com/id/1278139568/nl/foto/studioportret-van-20-jaar-oude-vrouw.jpg?s=612x612&w=0&k=20&c=3Bd4Ot79Z1ZKoCwAl0qFQ9hoBrQTar4SqtPefHOBEkg=', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'Bobby', photo: 'https://media.gettyimages.com/id/1250238624/nl/foto/handsome-young-adult-businessman-with-stubble.jpg?s=612x612&w=0&k=20&c=Ahojmst5H08S3yL7kaEj5mN3V8Weguk1V2pWDf4aH2Q=', bio: 'Teacher from Brazil.')
-third_user = User.create(name: 'John', photo: 'https://media.gettyimages.com/id/1092658864/nl/foto/confident-young-man-wearing-purple-t-shirt.jpg?s=612x612&w=0&k=20&c=Y8u4zM33AhjJczqOhQI5SM6-D3XzmZue8N1VIYPlmSA=', bio: 'Teacher from USA.')
+first_user = User.create(name: 'Lillian', photo: 'https://media.gettyimages.com/id/1278139568/nl/foto/studioportret-van-20-jaar-oude-vrouw.jpg?s=612x612&w=0&k=20&c=3Bd4Ot79Z1ZKoCwAl0qFQ9hoBrQTar4SqtPefHOBEkg=', bio: 'Teacher from Mexico.', posts_counter: 0)
 
-# Create a Post
-first_post = Post.create(user: first_user, title: 'Hello Microverse', text: 'This is my first post')
-second_post = Post.create(user: first_user, title: 'My best friend', text: 'This is my second post')
-third_post = Post.create(user: first_user, title: 'Enjoying all day long', text: 'This is my third post')
-fourth_post = Post.create(user: first_user, title: 'God is good', text: 'This is my fourth post')
+second_user = User.create(name: 'Bobby', photo: 'https://media.gettyimages.com/id/1250238624/nl/foto/handsome-young-adult-businessman-with-stubble.jpg?s=612x612&w=0&k=20&c=Ahojmst5H08S3yL7kaEj5mN3V8Weguk1V2pWDf4aH2Q=', bio: 'Teacher from Brazil.', posts_counter: 0)
 
-first_second_post = Post.create(user: second_user, title: 'Enjoying here', text: 'This is my first post by second user')
-second_second_post = Post.create(user: second_user, title: 'Having Fun', text: 'This is my second post by second user')
-third_second_post = Post.create(user: second_user, title: 'Thank God its weekend', text: 'This is my third post by second user')
+third_user = User.create(name: 'John', photo: 'https://media.gettyimages.com/id/1092658864/nl/foto/confident-young-man-wearing-purple-t-shirt.jpg?s=612x612&w=0&k=20&c=Y8u4zM33AhjJczqOhQI5SM6-D3XzmZue8N1VIYPlmSA=', bio: 'Teacher from USA.', posts_counter: 0)
 
-first_third_post = Post.create(user: third_user, title: 'My Exams', text: 'This is my first post by third user')
-second_third_post = Post.create(user: third_user, title: 'Swimming all day', text: 'This is my second post by third user')
 
-# Comments
+first_post = Post.create(user: first_user, title: 'Hello there', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+second_post = Post.create(user: first_user, title: 'Hello microverse', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
+third_post = Post.create(user: first_user, title: 'Hello Rails', text: 'This is my third post', comments_counter: 0, likes_counter: 0)
+fourth_post = Post.create(user: first_user, title: 'Hello Ruby', text: 'This is my fourth post', comments_counter: 0, likes_counter: 0)
+
 Comment.create(post: first_post, user: second_user, text: 'Hi Lillian!')
-Comment.create(post: first_post, user: second_user, text: 'How are you')
-Comment.create(post: first_post, user: second_user, text: 'Very nice')
-Comment.create(post: first_post, user: second_user, text: 'Good job dear')
+Comment.create(post: first_post, user: second_user, text: 'How are you?')
+Comment.create(post: first_post, user: second_user, text: 'You know Rails')
 
-Comment.create(post: first_second_post, user: third_user, text: 'Hi Bobby')
-Comment.create(post: first_second_post, user: third_user, text: 'Keep being you')
-Comment.create(post: first_second_post, user: third_user, text: 'Nice one')
 
+first_second_post = Post.create(user: second_user, title: 'Hello there', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+second_second_post = Post.create(user: second_user, title: 'Hello microverse', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
+
+first_third_post = Post.create(user: third_user, title: 'Hello Rails', text: 'This is my third post', comments_counter: 0, likes_counter: 0)
+second_tird_post = Post.create(user: third_user, title: 'Hello Ruby', text: 'This is my fourth post', comments_counter: 0, likes_counter: 0)
+
+Comment.create(post: first_second_post, user: third_user, text: 'Hi Bobby!')
 Comment.create(post: first_third_post, user: first_user, text: 'Hi John!')
-Comment.create(post: first_third_post, user: first_user, text: 'Success in your exams')
