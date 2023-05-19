401b2fb714900d64dd67a76ccd2b75065e333694
John Adibe <johnadibe123@gmail.com>
2023-05-18 16:17:22 -0800

    Add devise authorization to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    User.first
-  end
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 end
