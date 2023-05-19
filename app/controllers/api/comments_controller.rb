6d8b77bab00e6224cc864b0bfd4559081860e386
Donmark2k <chukwuemekavp@gmail.com>
2023-05-19 21:44:40 +0100

    Add comment controller API

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -4,4 +4,19 @@ class Api::CommentsController < ApplicationController
     @post = @user.posts.find(params[:post_id])
     render json: @post.comments
   end
+  def create
+    user = User.find(comment_params[:user_id])
+    post = Post.find(comment_params[:post_id])
+    new_comment =
+      Comment.new(user: user, post: post, text: comment_params[:text])
+    if new_comment.save
+      render json: 'Comment saved successfully', status: 200
+    else
+      render json: 'error saving comment', status: 400
+    end
+  end
+  private
+  def comment_params
+    params.require(:comment).permit(:user_id, :post_id, :text)
+  end
 end
