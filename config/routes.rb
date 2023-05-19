7d2d8af3ebb6c0c878843b086df25a548082add0
John Adibe <johnadibe123@gmail.com>
2023-05-18 16:13:30 -0800

    Add devise route in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'users#index'
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create, :new] do
       resources :comments, only: [:create, :new] 
-        resources :likes, only: [:create]
+      resources :likes, only: [:create]
     end
   end
-  root 'users#index'
 end
