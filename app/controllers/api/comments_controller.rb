a336b80e477f571a4b71557e680ae62196b6e98e
John Adibe <johnadibe123@gmail.com>
2023-05-19 20:43:16 -0800

    Fix linter errors

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,8 +1,7 @@
 class Api::CommentsController < ApplicationController
-  
-    def index
-      @user = User.find(params[:user_id])
-      @post = @user.posts.find(params[:post_id])
-      render json: @post.comments
-    end
+  def index
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    render json: @post.comments
+  end
 end
